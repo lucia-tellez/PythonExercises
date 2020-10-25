e67242da91d582d6edfe84276cc72f03e35c4d6d
lucia.tellez <l.tellez.012@gmail.com>
2020-10-25 17:56:11 +0000

    Exercises done

--- Exercises10.py
+++ Exercises10.py
@@ -15,3 +15,24 @@ reverseseq = shortDNA.replace("A", "t").replace("T","A").replace("G", "c").repla
 
 reverseseq.upper()
 
+#EXERCISE 3: Find the EcoRI cutting motif and give the length of the second half
+
+EcoDNA = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
+EcoDNA.find("GAATTC")
+SecondHalf = str(EcoDNA[21+1:])
+len(SecondHalf)
+
+##EXERCISE 4: Extract the exons from a sequence
+GenomicDNA = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+
+FirstExon = GenomicDNA[:61]
+
+SecondExon = GenomicDNA[90:]
+CodingSequence = FirstExon + SecondExon
+CodingPercentage = (len(CodingSequence)/len(GenomicDNA))*100
+print("The percentage of coding sequence in this DNA fragment is of", CodingPercentage)
+
+#Print the original genomic sequence with introns in lowercase
+Intron = GenomicDNA[61:89]
+IntronLC = Intron.lower()
+SequenceEX4 = FirstExon + IntronLC + SecondExon
