8d26a2d4957be491357d17e43ad82dcbe605bc92
lucia.tellez <l.tellez.012@gmail.com>
2020-10-25 17:11:43 +0000

    Exercise 2 done

--- Exercises10.py
+++ Exercises10.py
@@ -11,4 +11,7 @@ print("The AT content of the DNA sequence is", ATcontent)
 
 ##EXERCISE 2: Write the reverse complement of a DNA sequence
 
+reverseseq = shortDNA.replace("A", "t").replace("T","A").replace("G", "c").replace("C", "G")
+
+reverseseq.upper()
 
