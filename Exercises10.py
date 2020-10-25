b408270280f06f8af3aa469a4bd935cc3b76a315
lucia.tellez <l.tellez.012@gmail.com>
2020-10-25 16:46:22 +0000

    First exercise done! 23/10

---
+++ Exercises10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+##EXERCISE 1: Calculate AT content of a DNA sequence
+
+shortDNA = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+A = shortDNA.count("A")
+T = shortDNA.count("T")
+ATcontent = (A + T) / len(shortDNA)
+print("The AT content of the DNA sequence is", ATcontent)
+
+##EXERCISE 2: Write the reverse complement of a DNA sequence
+
+
